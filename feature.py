cceb3c9130b3e890a21e3692caecdd09b6cdb045
Peter Kekenes-Huskey <huskeypm@gmail.com>
2021-11-30 15:14:44 -0600

    added feature

--- feature.py
+++ feature.py
@@ -1,3 +1,27 @@
-def CalcMyFeature(variant):
-    return score
+
+"""
+This feature assigns a tag to the initial identity of an a.a.
+e.g. A-->X would return the value 0, based on the properties table below
+THe new data is assigned to a new dataframe tag (INITAA)
+"""
+def CalcInitialAA(df,newTag="INITAA"):
+
+  properties={
+      'a': 0, 'c': 1, 'd': 2, 'e': 3, 'f': 4,
+      'g': 5, 'h': 6, 'i': 7, 'k': 8, 'l': 9,
+      'm':10, 'n':11, 'p':12, 'q':13, 'r':14,
+      's':15, 't':16, 'v':17, 'w':18, 'y':19,
+  }
+
+  def calc_prop(variantName):
+    #print(variantName)
+    if variantName == 'wt':
+      return 20
+
+    daID = properties[variantName[0]]
+    return daID 
+
+  df[newTag] = df.apply( lambda row:
+         calc_prop(row['VARIANT']),
+         axis=1)
 
